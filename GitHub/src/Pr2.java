4412ed44b0f5e42120a2f3605d2c22244f5c5479
IEUser <IEUser@192.168.190.139>
2018-04-17 03:23:50 -0700

    V3 buena BUENA

--- GitHub/src/Pr2.java
+++ GitHub/src/Pr2.java
@@ -4,6 +4,8 @@ public class Pr2 {
 	public static void main(String[] args) {
 				System.out.println("Hola Git");
 				System.out.println("Adios Git");
-	}
+				System.out.println("Soy Santi y soy un manco al fortnite y además de ser un inutil");
+
+	}			
 
 }
